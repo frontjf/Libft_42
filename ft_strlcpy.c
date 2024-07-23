c0b53b02cde164bdc201e6d6d8596b436c57975d
frontjf <frontjf@gmail.com>
2024-07-23 22:17:24 +0200

    libft

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,14 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* #include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (!src)
+		return (0);
+	if (!dst)
+		return (ft_strlen(src));
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+} */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
-	if (!dst || !src)
+	if (!src)
 		return (0);
+	if (!dst)
+		return (ft_strlen(src));
 	i = 0;
 	if (dstsize > 0)
 	{
--- ft_substr.c
+++ ft_substr.c
@@ -16,11 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	s_len;
 
 	if (!s)
 		return (NULL);
-	if (start >= ft_strlen(s))
+	s_len = ft_strlen(s);
+	if (start >= s_len)
 		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
 	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (NULL);
